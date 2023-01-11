5ff46e29f873731ba14e6cb817ec790cb752a187
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-01-11 21:07:32 +0200

    Feature/ADD CSS & index File Description: Add the css and index.js files with the packages used.

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from "react";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {ToastContainer} from 'react-toastify';
+import Movies from "./Components/movies";
+import MovieForm from "./Components/movieForm";
+import NotFound from "./Components/notFound";
+import NavBar from "./Components/navBar";
+import LoginForm from "./Components/loginForm";
+import Logout from "./Components/logout";
+import ProtectedRoute from "./Components/common/protectedRoute";
+import auth from "./Services/authService";
+import 'react-toastify/dist/ReactToastify.css';
+import "./App.css";
+
+class App extends Component {
+    state = {};
+
+    componentDidMount() {
+        const user = auth.getCurrentUser();
+        this.setState({user});
+    }
+
+
+    render() {
+        const {user} = this.state;
+
+        return (
+            <div className={"main-content"}>
+                <React.Fragment>
+                    <ToastContainer/>
+                    <NavBar user={this.state.user}/>
+                    <main className={"container"}>
+                        <Routes>
+                            <Route path="/login" element={<LoginForm/>}/>
+                            <Route path="/logout" element={<Logout/>}/>
+                            <Route path="/movies/:id" element={<ProtectedRoute />} >
+                                <Route path='/movies/:id' element={<MovieForm />}/>
+                            </Route>
+                            <Route path="/movies" element={<Movies user={user}/>}/>
+                            <Route path="/not-found" element={<NotFound/>}/>
+                            <Route path="/" exact element={<Navigate to="/movies"/>}/>
+                            <Route path="*" element={<Navigate to="/not-found" replace/>}/>
+                        </Routes>
+                    </main>
+                </React.Fragment>
+            </div>
+        );
+    }
+}
+
+export default App;
