3549f03dce2b5f67e753dce2822281e6bb79c145
Mohamed Halfawy <mohamed.elhalfawyy@gmail.com>
2023-01-13 17:14:03 +0200

    BUGFIX/Add alt for notfound image

--- src/Components/notFound.jsx
+++ src/Components/notFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
     return(
         <div className={"test"}>
-            <center><img src={require("../images/error.gif")} className={"error"}/></center>
+            <center><img src={require("../images/error.gif")} className={"error"} alt={"Notfound Gif"}/></center>
         </div>
     );
 };
